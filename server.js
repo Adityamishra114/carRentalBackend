9eba189b0237b5d3ea6b3eb335d553d8bb4c18a4
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-10-24 22:33:30 +0530

    g

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const CLIENT_URL =
 
 app.use(
   cors({
-    origin:"*",
+    origin:CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
